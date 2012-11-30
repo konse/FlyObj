414023f07b455d50bda99e8cace6c53b3c546b6e
konse <yasdelaniz@gmail.com>
2012-12-01 00:22:16 +0400

    предложение

--- src/WIndowsButton1.java
+++ src/WIndowsButton1.java
@@ -11,7 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashSet;
 import java.util.Set;
-
+// предлагаю ввести переписку
 //яяяяяяяяяяяяяяяяяяяяяяяя
 
 class FlyObj{
